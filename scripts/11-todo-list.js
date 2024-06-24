d1ba9d347a56a5bc6616452b810f0e5415edc916
*AbhinavSingh93* <*abhinavsingh18813@gmail.com*>
2024-06-25 02:21:58 +0530

    Todo-Project

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,10 +1,47 @@
 const todoList=[];
 
+renderTodoList();
+function renderTodoList()
+{
+  let todoListHtml='';
+  for(let i=0;i<todoList.length;i++)
+    {
+      const todoObject=(todoList[i]);
+      /*const name=todoObject.name;
+      const dueDate=todoObject.dueDate;*/
+      const {name,dueDate}=todoObject;
+      //generating the html
+      const html=`
+      <div>${name}</div>
+       <div>${dueDate}</div>
+      <button onclick="
+      todoList.splice(${i},1);
+      renderTodoList();
+      " class="delete-todo-button">Delete</button>
+      `;
+      todoListHtml+=html;
+    }
+   
+    document.querySelector('.js-todo-list')
+    .innerHTML=todoListHtml;
+}
 function addTodo(){
   const inputElement=document.querySelector('.js-name-input');
   const name=inputElement.value;
-  todoList.push(name);
-  console.log(todoList);
+
+  const dateInputElement=document.querySelector('.js-due-date-input');
+
+  const dueDate=dateInputElement.value;
+
+  todoList.push({
+    /*name:name,
+    dueDate:dueDate*/
+    name,
+    dueDate
+});
+
 
   inputElement.value="";
+
+  renderTodoList();
 }
